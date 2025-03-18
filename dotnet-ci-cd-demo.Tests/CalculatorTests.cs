8955388543c39451241847bb17e7f8d1abd76331
davinceleecode <vincent.lee.c.flores@gmail.com>
2025-03-18 14:42:39 +0800

    Modify the subtract parameter value

--- dotnet-ci-cd-demo.Tests/CalculatorTests.cs
+++ dotnet-ci-cd-demo.Tests/CalculatorTests.cs
@@ -27,10 +27,10 @@ namespace dotnet_ci_cd_demo.Tests
             var calc = new Calculator();
 
             //Act
-            int result = calc.Subtract(7, 5);
+            int result = calc.Subtract(5, 5);
 
             //Assert
-            Assert.Equal(2, result);
+            Assert.Equal(0, result);
         }
     }
 }
